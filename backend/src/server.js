5a844365188340d72098426fbb3b34cbf3e5dc80
Hamza Khalid <hamzakhalid1067@gmail.com>
2025-12-27 20:59:31 +0500

    Initialize admin, backend and mobile directories in the repo

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,15 @@
 import express from "express"
+import path from "path"
 
 const app = express()
+const __dirname = path.resolve()
 
 app.get("/api/health", (req, res) => {
     res.status(200).json({ message: "Success"})
 })
 
+// make our app ready for deployment
+
 app.listen(3000, () => {
-    console.log("Server is up and running")
+    console.log("Server is up and running123")
 })

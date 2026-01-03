3744f1e176158c50c3343e481c9b35d28b060d6e
Hamza Khalid <hamzakhalid1067@gmail.com>
2026-01-03 22:12:26 +0500

    setup all the directories

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,24 @@
-import express from "express"
-import path from "path"
+import express from "express";
+import path from "path";
+import { ENV } from "./config/env.js";
 
-const app = express()
-const __dirname = path.resolve()
+const app = express();
+const __dirname = path.resolve();
 
 app.get("/api/health", (req, res) => {
-    res.status(200).json({ message: "Success"})
-})
+  res.status(200).json({ message: "Success" });
+});
 
 // make our app ready for deployment
 
-app.listen(3000, () => {
-    console.log("Server is up and running123")
-})
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../admin/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
+  });
+}
+
+app.listen(ENV.PORT, () => {
+  console.log("Server is up and running");
+});
